25850adb069da9ba433cda133c3e185b379a721e
Harry <RedAndBlueEraser@outlook.com>
2017-07-15 14:45:55 -0700

    Added cocktail shaker sort algorithm.

--- sort.c
+++ sort.c
@@ -12,6 +12,9 @@
 /* Sort the elements in the array with bubble sort. */
 void sort_bubblesort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
 
+/* Sort the elements in the array with cocktail shaker sort. */
+void sort_cocktailshakersort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
+
 /* Print integer array. */
 void printintarray(int arr[], size_t len) {
     size_t i;
@@ -52,7 +55,7 @@ int main(int argc, char *argv[]) {
 
     /* Print array before and after sort. */
     printintarray(arr, ARRLEN);
-    sort_bubblesort(arr, 10, sizeof (int), cmp);
+    sort_cocktailshakersort(arr, 10, sizeof (int), cmp);
     printintarray(arr, ARRLEN);
 
     return 0;
@@ -82,5 +85,45 @@ void sort_bubblesort(void *arr, size_t count, size_t elesize, int (*cmp)(const v
                 memswap(ptr1, ptr2, elesize);
             }
         }
+    }
+}
+
+void sort_cocktailshakersort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *)) {
+    char *ptrstart = (char *)arr,  /* Pointer to first element in array. */
+        *ptrend,                   /* Pointer to last element in array. */
+        *ptr1,                     /* Pointer to first element to be compared. */
+        *ptr2;                     /* Pointer to second element to be compared. */
+
+    /* Avoid unsigned arithmetic loss. */
+    if (count == 0) {
+        return;
+    }
+
+    /* Iterate until array sorted. After the n'th iteration, the n smallest and
+     * n largest elements would be correctly sorted at the end of the array.
+     */
+    ptrend = ptrstart + (count - 1) * elesize;
+    while (ptrstart < ptrend) {
+        /* Iterate pairs until reach sorted end of array. */
+        for (ptr1 = ptrstart, ptr2 = ptr1 + elesize; ptr1 < ptrend; ptr1 = ptr2, ptr2 = ptr1 + elesize) {
+            /* Compare pair and swap larger element towards the end of the
+             * array.
+             */
+            if (cmp(ptr1, ptr2) > 0) {
+                memswap(ptr1, ptr2, elesize);
+            }
+        }
+        ptrend -= elesize;
+
+        /* Iterate pairs until reach sorted start of array. */
+        for (ptr2 = ptrend, ptr1 = ptr2 - elesize; ptr2 > ptrstart; ptr2 = ptr1, ptr1 = ptr2 - elesize) {
+            /* Compare pair and swap larger element towards the end of the
+             * array.
+             */
+            if (cmp(ptr1, ptr2) > 0) {
+                memswap(ptr1, ptr2, elesize);
+            }
+        }
+        ptrstart += elesize;
     }
 }
