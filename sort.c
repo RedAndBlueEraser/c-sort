c384ba866b8f1125f31cd8890a154ff2a62cac0c
Harry <RedAndBlueEraser@outlook.com>
2017-07-15 14:45:18 -0700

    Added bubble sort algorithm.

--- sort.c
+++ sort.c
@@ -9,6 +9,9 @@
 
 #define ARRLEN 10
 
+/* Sort the elements in the array with bubble sort. */
+void sort_bubblesort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
+
 /* Print integer array. */
 void printintarray(int arr[], size_t len) {
     size_t i;
@@ -32,13 +35,52 @@ int cmp(const void *a, const void *b) {
     return *(int *)b - *(int *)a;
 }
 
+/* Swap the specified amount of bytes between two memory areas. */
+void memswap(void *ptr1, void *ptr2, size_t len) {
+    char *a = ptr1, *aend = a + len, *b = ptr2, tmp;
+    while (a < aend) {
+        tmp = *a;
+        *a = *b;
+        *b = tmp;
+        a++;
+        b++;
+    }
+}
+
 int main(int argc, char *argv[]) {
     int arr[ARRLEN] = {380, -751, 812, 542, 436, -300, 794, -978, -870, 711};
 
     /* Print array before and after sort. */
     printintarray(arr, ARRLEN);
-    qsort(arr, 10, sizeof (int), cmp);
+    sort_bubblesort(arr, 10, sizeof (int), cmp);
     printintarray(arr, ARRLEN);
 
     return 0;
+}
+
+void sort_bubblesort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *)) {
+    char *ptrstart = (char *)arr,  /* Pointer to first element in array. */
+        *ptrend,                   /* Pointer to last element in array. */
+        *ptr1,                     /* Pointer to first element to be compared. */
+        *ptr2;                     /* Pointer to second element to be compared. */
+
+    /* Avoid unsigned arithmetic loss. */
+    if (count == 0) {
+        return;
+    }
+
+    /* Iterate until array sorted. After the n'th iteration, the n largest
+     * elements would be correctly sorted at the end of the array.
+     */
+    for (ptrend = ptrstart + (count - 1) * elesize; ptrstart < ptrend; ptrend -= elesize) {
+        /* Iterate pairs until reach sorted end of array. */
+        for (ptr1 = ptrstart, ptr2 = ptr1 + elesize; ptr1 < ptrend; ptr1 = ptr2, ptr2 = ptr1 + elesize) {
+            /* Compare pair and swap larger element towards the end of the
+             * array.
+             */
+            if (cmp(ptr1, ptr2) > 0) {
+                memswap(ptr1, ptr2, elesize);
+            }
+        }
+    }
 }
