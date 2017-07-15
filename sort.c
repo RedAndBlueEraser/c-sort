92a31acd7f6dc6446decc2712690e36fd4cbb9f8
Harry <RedAndBlueEraser@outlook.com>
2017-07-14 18:03:10 -0700

    Initial commit with boilerplate C program.

---
+++ sort.c
@@ -0,0 +1,12 @@
+/*
+ * sort.c
+ * Version 20170714
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
