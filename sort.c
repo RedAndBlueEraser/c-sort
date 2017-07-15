d73c60f76a26f92775a632f2234c8960e1d7c534
Harry <RedAndBlueEraser@outlook.com>
2017-07-14 20:59:19 -0700

    Added rough layout of program.

--- sort.c
+++ sort.c
@@ -5,8 +5,40 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ARRLEN 10
+
+/* Print integer array. */
+void printintarray(int arr[], size_t len) {
+    size_t i;
+
+    printf("[");
+
+    /* Print first element if it exists. */
+    if (len > 0) {
+        printf("%d", arr[0]);
+    }
+    /* Print subsequent elements padded with a comma and space. */
+    for (i = 1; i < len; i++) {
+        printf(", %d", arr[i]);
+    }
+
+    printf("] @ %p\n", arr);
+}
+
+/* Comparison function to determine the sort order. */
+int cmp(const void *a, const void *b) {
+    return *(int *)b - *(int *)a;
+}
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    int arr[ARRLEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    /* Print array before and after sort. */
+    printintarray(arr, ARRLEN);
+    qsort(arr, 10, sizeof (int), cmp);
+    printintarray(arr, ARRLEN);
+
     return 0;
 }
