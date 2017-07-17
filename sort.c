70a1418db4f88fa3d641defaa9cf2336801bc058
Harry <RedAndBlueEraser@outlook.com>
2017-07-17 16:05:53 -0700

    Added sort.h header file for sort.c source file.

--- sort.c
+++ sort.c
@@ -6,26 +6,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "sort.h"
 
 #define ARRLEN 10
 #define FALSE 0
 #define TRUE !FALSE
 #define SORT_COMBSORT_SHRINKFACTOR 1.3
-
-/* Sort the elements in the array with bubble sort. */
-void sort_bubblesort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
-
-/* Sort the elements in the array with cocktail shaker sort. */
-void sort_cocktailshakersort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
-
-/* Sort the elements in the array with odd-even sort. */
-void sort_oddevensort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
-
-/* Sort the elements in the array with comb sort. */
-void sort_combsort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
-
-/* Sort the elements in the array with gnome sort. */
-void sort_gnomesort(void *arr, size_t count, size_t elesize, int (*cmp)(const void *, const void *));
 
 /* Print integer array. */
 void printintarray(int arr[], size_t len) {
---
+++ sort.h
@@ -0,0 +1,27 @@
+/*
+ * sort.h
+ * Version 20170714
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+#include <stddef.h>
+
+/* Sort the elements in the array with bubble sort. */
+void sort_bubblesort(void *arr, size_t count, size_t elesize,
+    int (*cmp)(const void *, const void *));
+
+/* Sort the elements in the array with cocktail shaker sort. */
+void sort_cocktailshakersort(void *arr, size_t count, size_t elesize,
+    int (*cmp)(const void *, const void *));
+
+/* Sort the elements in the array with odd-even sort. */
+void sort_oddevensort(void *arr, size_t count, size_t elesize,
+    int (*cmp)(const void *, const void *));
+
+/* Sort the elements in the array with comb sort. */
+void sort_combsort(void *arr, size_t count, size_t elesize,
+    int (*cmp)(const void *, const void *));
+
+/* Sort the elements in the array with gnome sort. */
+void sort_gnomesort(void *arr, size_t count, size_t elesize,
+    int (*cmp)(const void *, const void *));
